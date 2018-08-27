77112a1f3580758e008d90ee0fe53327fb62d288
adi-howdy <adityakhatri123@gmail.com>
2018-08-26 18:02:19 -0700

    simple node example

---
+++ index.js
@@ -0,0 +1,20 @@
+var rect = {
+    per: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solve(l,b){
+    console.log("solvin log " + l + " and " + b)
+
+    if(l<=0 || b<=0){
+        console.log("error in input")
+    }
+    else 
+    {
+        console.log("area " + rect.area(l,b))
+        console.log("per: " + rect.per(l,b))
+    }
+}
+
+solve(2,3);
+solve(0,2);
