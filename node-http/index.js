21c9ed01b92abfb7dc8636ae6ad0bf4da8c62739
adi-howdy <adityakhatri123@gmail.com>
2018-08-27 19:40:21 -0700

    creating http server with index.html page and aboutus.html page

--- node-http/index.js
+++ node-http/index.js
@@ -1,14 +1,56 @@
 // require is used to include http module in javascript
 const http = require('http');
+const fs = require('fs'); // allow to read file sytem
+const path = require('path'); // for path in the file system
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req,res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>TQ</h1></body></html>');
+    console.log("Request for " +  req.url  + " by method " + req.method);
+    var fileUrl;
+    if(req.method == 'GET'){
+        if(req.url == '/') 
+        {
+            fileUrl = '/index.html'
+        }
+        else{
+            fileUrl = req.url;
+            }
+        var filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+
+        //if file html exist it will take that html file else it will give file not found error
+        if(fileExt == '.html'){
+            fs.exists(filePath, (exists) => {
+                if (!exists){
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html> <body><p>file not found</p></body></html>');
+                    return;
+                }
+                else 
+                {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'text/html');
+                    fs.createReadStream(filePath).pipe(res); //create a stream of byte and send it as response
+                }
+            } )
+        }
+        else {
+            res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html> <body><p>html file not found</p></body></html>');
+                    return;
+        }
+    }
+    else{
+        res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html> <body><p>method not supported</p></body></html>');
+                    return;
+    }
+    
 })
 
 
